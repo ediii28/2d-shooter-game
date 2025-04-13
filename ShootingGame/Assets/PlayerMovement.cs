1e751634f07fd2425e4d94f4cb34ef03e3c387e7
UWantMyName <stefantanasa13@gmail.com>
2025-04-13 12:18:55 +0300

    Refactored player movement so that the player is afected by physics.

--- ShootingGame/Assets/PlayerMovement.cs
+++ ShootingGame/Assets/PlayerMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Rigidbody2D rb;
     public float movSpeed = 5f;
 
     void Update()
@@ -12,27 +13,32 @@ public class PlayerMovement : MonoBehaviour
         // Daca apas RightArrow, merg spre dreapta
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.position += Vector3.left * movSpeed * Time.deltaTime;
+            rb.position += Vector2.left * movSpeed * Time.deltaTime;
             // Vector3.left = (-1, 0, 0)
             // directie * viteza * deltaTime
         }
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.position += Vector3.right * movSpeed * Time.deltaTime;
+            rb.position += Vector2.right * movSpeed * Time.deltaTime;
             // Vector3.right = (1, 0, 0)
         }
 
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            transform.position += Vector3.up * movSpeed * Time.deltaTime;
+            rb.position += Vector2.up * movSpeed * Time.deltaTime;
             // Vector3.up = (0, 1, 0)
         }
 
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            transform.position += Vector3.down * movSpeed * Time.deltaTime;
+            rb.position += Vector2.down * movSpeed * Time.deltaTime;
             // Vector3.down = (0, -1, 0)
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print(collision.gameObject.name);
     }
 }
